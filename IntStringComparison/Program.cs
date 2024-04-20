0ed51de3890ad0e9b2424581d29320b80110fd76
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-04-20 23:08:04 +0200

    refactor: extracted methods for string and int measurement

--- IntStringComparison/Program.cs
+++ IntStringComparison/Program.cs
@@ -1,7 +1,42 @@
-﻿namespace IntStringComparison;
+﻿using System.Diagnostics;
+
+namespace IntStringComparison;
 
 class Program {
-	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
+	private const string DummyDictionaryItem = "Potato";
+
+	private static long _nanosecPerTick = 1000000000L / Stopwatch.Frequency;
+	private const string TimeMeasureUnits = "ns";
+
+	private static void Main() {
+		CalculateIntDictionary();
+		CalculateStringDictionary();
+	}
+
+	private static void CalculateIntDictionary() {
+		Random rng = new();
+		RandomDictionary<int, string> dictionaryInt = new(10000000, GenerateInt, () => DummyDictionaryItem);
+
+		Stopwatch watch = Stopwatch.StartNew();
+		dictionaryInt.Iterate();
+		watch.Stop();
+		long elapsed = watch.ElapsedTicks * _nanosecPerTick;
+		Console.WriteLine($"Time elapsed (int): {elapsed}{TimeMeasureUnits}");
+		return;
+
+		int GenerateInt() => rng.Next();
+	}
+
+	private static void CalculateStringDictionary() {
+		RandomDictionary<string, string> dictionaryString = new(10000000, GenerateGuid, () => DummyDictionaryItem);
+
+		Stopwatch watch = Stopwatch.StartNew();
+		dictionaryString.Iterate();
+		watch.Stop();
+		long elapsed = watch.ElapsedTicks * _nanosecPerTick;
+		Console.WriteLine($"Time elapsed (string): {elapsed}{TimeMeasureUnits}");
+		return;
+
+		string GenerateGuid() => Guid.NewGuid().ToString();
 	}
 }
--- IntStringComparison/RandomDictionary.cs
+++ IntStringComparison/RandomDictionary.cs
@@ -1,5 +1,21 @@
 ﻿namespace IntStringComparison;
 
-public class RandomDictionary {
-	
+public class RandomDictionary<K, V> {
+	private readonly List<K> _keys = [];
+
+	private readonly Dictionary<K, V> _dictionary = new();
+
+	public RandomDictionary(int members, Func<K> keyGenerator, Func<V> valueGenerator) {
+		for (int i = 0; i < members; i++) {
+			K key = keyGenerator.Invoke();
+			while (_dictionary.TryAdd(key, valueGenerator.Invoke()) == false) {
+				key = keyGenerator.Invoke();
+			}
+			_keys.Add(key);
+		}
+	}
+
+	public void Iterate() {
+		foreach (V _ in _keys.Select(key => _dictionary[key])) { }
+	}
 }
