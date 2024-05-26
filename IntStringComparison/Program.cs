827b6cc286c3c3c4182a650df596b4441041d7dc
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-05-26 22:05:25 +0200

    feat: test runs

--- IntStringComparison/Program.cs
+++ IntStringComparison/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Text;
+using TextCopy;
 
 namespace IntStringComparison;
 
@@ -9,8 +11,7 @@ internal abstract class Program {
 		DateTime start = DateTime.Now;
 		Console.WriteLine("### Overall Start Time: " + start.ToLongTimeString());
 		Console.WriteLine();
-		Random rand = new();
-		MeasureDifference(rand.Next(0, MaxNumbersToTest));
+		RunTest();
 		DateTime end = DateTime.Now;
 		Console.WriteLine();
 		Console.WriteLine("### Overall End Time: " + end.ToLongTimeString());
@@ -20,7 +21,33 @@ internal abstract class Program {
 		Console.ReadLine();
 	}
 
-	private static void MeasureDifference(int numberOfNumbersToTest) {
+	private static void RunTest() {
+		const int testRuns = 1000;
+
+		TestResult[] results = new TestResult[testRuns];
+
+		Random rand = new();
+		for (int i = 0; i < testRuns; i++) {
+			Console.WriteLine($"Test Run no. {i + 1} out of {testRuns}");
+			MeasureDifference(rand.Next(0, MaxNumbersToTest), out results[i]);
+			GC.Collect();
+		}
+
+		Console.WriteLine();
+		Console.WriteLine("###########################################################");
+		Console.WriteLine("######## Test Results");
+		StringBuilder resultBuilder = new("iAdd\tsAdd\tiRead\tsRead\tsize\n");
+		Console.WriteLine("   Int Add   |  String Add  |   Int Read   |  String Read |   Size");
+		foreach (TestResult result in results) {
+			resultBuilder.AppendLine(result.ToString());
+			Console.WriteLine(result.ToString());
+		}
+		ClipboardService.SetText(resultBuilder.ToString());
+		Console.WriteLine();
+		Console.WriteLine("Result CSV copied to clipboard");
+	}
+
+	private static void MeasureDifference(int numberOfNumbersToTest, out TestResult result) {
 		Console.WriteLine($"######## {nameof(MeasureDifference)}");
 		Console.WriteLine($"Number of values to convert: {numberOfNumbersToTest:#,##0}");
 
@@ -29,101 +56,9 @@ internal abstract class Program {
 		string[] s = new string[numberOfNumbersToTest];
 		int[] i = new int[numberOfNumbersToTest];
 
-		Dictionary<string, int> ds = new();
-		Dictionary<int, int> di = new();
+		Dictionary<string, int> ds = new(numberOfNumbersToTest);
+		Dictionary<int, int> di = new(numberOfNumbersToTest);
 
-		InitializeArrays(numberOfNumbersToTest, i, s);
-		Thread.Sleep(500);
-
-		string addIntElapsed = MeasureAddInt(numberOfNumbersToTest, sw, di, i);
-		Thread.Sleep(500);
-
-		string addStringElapsed = MeasureAddString(numberOfNumbersToTest, sw, ds, s, i);
-		Thread.Sleep(500);
-
-		string readIntElapsed = MeasureReadInt(numberOfNumbersToTest, sw, di, i);
-		Thread.Sleep(500);
-
-		string readStringElapsed = MeasureReadString(numberOfNumbersToTest, sw, ds, s);
-		Thread.Sleep(500);
-
-		Array.Clear(i, 0, i.Length);
-		Array.Clear(s, 0, s.Length);
-		di.Clear();
-		ds.Clear();
-
-		Console.WriteLine("###########################################################");
-		Console.WriteLine("Results Summary");
-		Console.WriteLine($"Number of dictionary elements: {numberOfNumbersToTest:#,##0}");
-		Console.WriteLine("Add int to dictionary: " + addIntElapsed);
-		Console.WriteLine("Add string to dictionary: " + addStringElapsed);
-		Console.WriteLine("Read int from dictionary: " + readIntElapsed);
-		Console.WriteLine("Read string from dictionary: " + readStringElapsed);
-	}
-
-	private static string MeasureReadString(int numberOfNumbersToTest, Stopwatch sw, IReadOnlyDictionary<string, int> ds, IReadOnlyList<string> s) {
-		Console.WriteLine("###########################################################");
-		Console.WriteLine("Starting reading stringified-int-key from dictionary test " +
-		                  numberOfNumbersToTest.ToString("#,##0") + " at: " + DateTime.Now.ToLongTimeString());
-		long total = 0;
-		sw.Restart();
-		for (int x = 0; x < numberOfNumbersToTest; x++)
-			total += ds[s[x]];
-		sw.Stop();
-		Console.WriteLine("Finished at: " + DateTime.Now.ToLongTimeString());
-		Console.WriteLine("Total: " + total.ToString("#,##0"));
-		string readStringElapsed = sw.Elapsed.ToString(@"mm\:ss\.fffffff");
-		Console.WriteLine("Time to run: " + readStringElapsed);
-		return readStringElapsed;
-	}
-
-	private static string MeasureReadInt(int numberOfNumbersToTest, Stopwatch sw, IReadOnlyDictionary<int, int> di, IReadOnlyList<int> i) {
-		Console.WriteLine("###########################################################");
-		Console.WriteLine("Starting reading int-key from dictionary test " +
-		                  numberOfNumbersToTest.ToString("#,##0") + " at: " + DateTime.Now.ToLongTimeString());
-		long total = 0;
-		sw.Restart();
-		for (int x = 0; x < numberOfNumbersToTest; x++)
-			total += di[i[x]];
-		sw.Stop();
-		Console.WriteLine("Finished at: " + DateTime.Now.ToLongTimeString());
-		Console.WriteLine("Total: " + total.ToString("#,##0"));
-		string readIntElapsed = sw.Elapsed.ToString(@"mm\:ss\.fffffff");
-		Console.WriteLine("Time to run: " + readIntElapsed);
-		return readIntElapsed;
-	}
-
-	private static string MeasureAddString(int numberOfNumbersToTest, Stopwatch sw, IDictionary<string, int> ds, IReadOnlyList<string> s, IReadOnlyList<int> i) {
-		Console.WriteLine("###########################################################");
-		Console.WriteLine("Starting adding stringified-int-key to dictionary test " +
-		                  numberOfNumbersToTest.ToString("#,##0") + " at: " + DateTime.Now.ToLongTimeString());
-		sw.Restart();
-		for (int x = 0; x < numberOfNumbersToTest; x++)
-			ds.Add(s[x], i[x]);
-		sw.Stop();
-		Console.WriteLine("Finished at: " + DateTime.Now.ToLongTimeString());
-		Console.WriteLine("Number of elements: " + ds.Count.ToString("#,##0"));
-		string addStringElapsed = sw.Elapsed.ToString(@"mm\:ss\.fffffff");
-		Console.WriteLine("Time to run: " + addStringElapsed);
-		return addStringElapsed;
-	}
-
-	private static string MeasureAddInt(int numberOfNumbersToTest, Stopwatch sw, IDictionary<int, int> di, IReadOnlyList<int> i) {
-		Console.WriteLine("###########################################################");
-		Console.WriteLine("Starting adding int-key to dictionary test " + numberOfNumbersToTest.ToString("#,##0") +
-		                  " at: " + DateTime.Now.ToLongTimeString());
-		sw.Restart();
-		for (int x = 0; x < numberOfNumbersToTest; x++)
-			di.Add(i[x], i[x]);
-		sw.Stop();
-		Console.WriteLine("Finished at: " + DateTime.Now.ToLongTimeString());
-		Console.WriteLine("Number of elements: " + di.Count.ToString("#,##0"));
-		string addIntElapsed = sw.Elapsed.ToString(@"mm\:ss\.fffffff");
-		Console.WriteLine("Time to run: " + addIntElapsed);
-		return addIntElapsed;
-	}
-
-	private static void InitializeArrays(int numberOfNumbersToTest, int[] i, IList<string> s) {
 		Console.WriteLine("Initializing int items array...");
 		for (int x = 0; x < numberOfNumbersToTest; x++) {
 			i[x] = x;
@@ -141,5 +76,76 @@ internal abstract class Program {
 		}
 		Console.WriteLine("Integers converted.");
 		Console.WriteLine();
+		Thread.Sleep(500);
+
+		Console.WriteLine("###########################################################");
+		Console.WriteLine("Starting adding int-key to dictionary test " + numberOfNumbersToTest.ToString("#,##0") +
+		                  " at: " + DateTime.Now.ToLongTimeString());
+		sw.Restart();
+		for (int x1 = 0; x1 < numberOfNumbersToTest; x1++)
+			di.Add(i[x1], i[x1]);
+		sw.Stop();
+		Console.WriteLine("Finished at: " + DateTime.Now.ToLongTimeString());
+		Console.WriteLine("Number of elements: " + di.Count.ToString("#,##0"));
+		string addIntElapsed = sw.Elapsed.ToString(@"mm\:ss\.fffffff");
+		Console.WriteLine("Time to run: " + addIntElapsed);
+		Thread.Sleep(500);
+
+		Console.WriteLine("###########################################################");
+		Console.WriteLine("Starting adding stringified-int-key to dictionary test " +
+		                  numberOfNumbersToTest.ToString("#,##0") + " at: " + DateTime.Now.ToLongTimeString());
+		sw.Restart();
+		for (int x2 = 0; x2 < numberOfNumbersToTest; x2++)
+			ds.Add(s[x2], i[x2]);
+		sw.Stop();
+		Console.WriteLine("Finished at: " + DateTime.Now.ToLongTimeString());
+		Console.WriteLine("Number of elements: " + ds.Count.ToString("#,##0"));
+		string addStringElapsed = sw.Elapsed.ToString(@"mm\:ss\.fffffff");
+		Console.WriteLine("Time to run: " + addStringElapsed);
+		Thread.Sleep(500);
+
+		Console.WriteLine("###########################################################");
+		Console.WriteLine("Starting reading int-key from dictionary test " +
+		                  numberOfNumbersToTest.ToString("#,##0") + " at: " + DateTime.Now.ToLongTimeString());
+		long total = 0;
+		sw.Restart();
+		for (int x3 = 0; x3 < numberOfNumbersToTest; x3++)
+			total += di[i[x3]];
+		sw.Stop();
+		Console.WriteLine("Finished at: " + DateTime.Now.ToLongTimeString());
+		Console.WriteLine("Total: " + total.ToString("#,##0"));
+		string readIntElapsed = sw.Elapsed.ToString(@"mm\:ss\.fffffff");
+		Console.WriteLine("Time to run: " + readIntElapsed);
+		Thread.Sleep(500);
+
+		Console.WriteLine("###########################################################");
+		Console.WriteLine("Starting reading stringified-int-key from dictionary test " +
+		                  numberOfNumbersToTest.ToString("#,##0") + " at: " + DateTime.Now.ToLongTimeString());
+		long total1 = 0;
+		sw.Restart();
+		for (int x4 = 0; x4 < numberOfNumbersToTest; x4++)
+			total1 += ds[s[x4]];
+		sw.Stop();
+		Console.WriteLine("Finished at: " + DateTime.Now.ToLongTimeString());
+		Console.WriteLine("Total: " + total1.ToString("#,##0"));
+		string readStringElapsed = sw.Elapsed.ToString(@"mm\:ss\.fffffff");
+		Console.WriteLine("Time to run: " + readStringElapsed);
+		Thread.Sleep(500);
+
+		Array.Clear(i);
+		Array.Clear(s);
+		di.Clear();
+		ds.Clear();
+
+		result = new TestResult(addIntElapsed, addStringElapsed, readIntElapsed, readStringElapsed,
+			numberOfNumbersToTest.ToString("#,##0"));
+
+		Console.WriteLine("###########################################################");
+		Console.WriteLine("Results Summary");
+		Console.WriteLine($"Number of dictionary elements: {numberOfNumbersToTest:#,##0}");
+		Console.WriteLine("Add int to dictionary: " + addIntElapsed);
+		Console.WriteLine("Add string to dictionary: " + addStringElapsed);
+		Console.WriteLine("Read int from dictionary: " + readIntElapsed);
+		Console.WriteLine("Read string from dictionary: " + readStringElapsed);
 	}
 }
---
+++ IntStringComparison/TestResult.cs
@@ -0,0 +1,5 @@
+﻿namespace IntStringComparison;
+
+public readonly struct TestResult(string intAddTime, string stringAddTime, string intReadTime, string stringReadTime, string elementsTested) {
+	public override string ToString() => $"{intAddTime}\t{stringAddTime}\t{intReadTime}\t{stringReadTime}\t{elementsTested}";
+}
